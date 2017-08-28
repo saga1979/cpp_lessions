a15c1fd4929c5052b5a05a6297f509fc4d382024
saga1979 <zhuimeng79@msn.com>
2017-08-28 11:41:19 +0800

    可以按照任意顺序增加客户，而不是以前只能按客户编号

--- cpp/chap_11_file.h
+++ cpp/chap_11_file.h
@@ -1,16 +1,20 @@
 ﻿#pragma once
 
 #define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>
+#include <cstdio>
+#include <cstdbool>
 
+static const int MAX_NUM = 100;
 
-struct clientData
+typedef struct _ClientData
 {
 	int acctNum;
 	char lastName[15];
 	char firstName[10];
 	double balance;
-};
+}ClientData;
+
+static ClientData s_clients[MAX_NUM] = { 0 };
 
 int enterChoice(void);
 void textFile(FILE *);
@@ -18,15 +22,36 @@ void updateRecord(FILE *);
 void newRecord(FILE *);
 void deleteRecord(FILE *);
 
+void initialize(FILE *);
+
+int  recordIndex(int);
+
 int main()
 {
 	FILE *cfPtr = 0;
-
-	if ((cfPtr = fopen("credit.dat", "a+")) == NULL)
+	//对文件是否存在分情况处理
+	if ((cfPtr = fopen("credit.dat", "r")) == NULL)
 	{
 		printf("File could not be opened.\n");
-		return -1;
+		cfPtr = fopen("credit.dat", "w+");
+		if (cfPtr == 0)
+		{
+			perror("create file:");
+			return -1;
+		}
 	}
+	else
+	{
+		fclose(cfPtr);
+		cfPtr = fopen("credit.dat", "r+");
+		if (cfPtr == 0)
+		{
+			perror("create file:");
+			return -1;
+		}
+	}
+
+	initialize(cfPtr);
 
 	int choice;
 	while ((choice = enterChoice()) != 5)
@@ -55,10 +80,36 @@ int main()
 	return 0;
 }
 
+void initialize(FILE *file)
+{
+	int count = 0;
+	while (!feof(file) && count < MAX_NUM)
+	{
+		size_t ret = fread(&s_clients[count], sizeof(ClientData), 1, file);
+		if (ret != 1)
+		{
+			break;
+		}
+		count++;
+	}
+}
+
+int recordIndex(int account)
+{
+	for (int i = 0; i < MAX_NUM; i++)
+	{
+		if (s_clients[i].acctNum == account)
+		{
+			return i;
+		}
+	}
+	return  -1;
+}
+
 void textFile(FILE *readPtr)
 {
 	FILE *writePtr = 0;
-	struct clientData client = { 0, "", "", 0.0 };
+	ClientData client = { 0, "", "", 0.0 };
 
 	if ((writePtr = fopen("accounts.txt", "w")) == NULL)
 	{
@@ -72,7 +123,7 @@ void textFile(FILE *readPtr)
 
 		while (!feof(readPtr))
 		{
-			size_t ret = fread(&client, sizeof(struct clientData), 1,
+			size_t ret = fread(&client, sizeof(ClientData), 1,
 				readPtr);
 			if (ret != 1)
 			{
@@ -94,93 +145,89 @@ void updateRecord(FILE *fPtr)
 {
 	int account;
 	double transaction;
-	struct clientData client = { 0, "", "", 0.0 };
 
 	printf("Enter account to update ( 1 - 100 ): ");
 	scanf("%d", &account);
-	fseek(fPtr,
-		(account - 1) * sizeof(struct clientData),
-		SEEK_SET);
-	fread(&client, sizeof(struct clientData), 1, fPtr);
 
-	if (client.acctNum == 0)
+	int index = recordIndex(account);
+	if(-1 == index )
 	{
 		printf("Acount #%d has no information.\n", account);
+		return;
 	}
-	else 
-	{
-		printf("%-6d%-16s%-11s%10.2f\n\n",
-			client.acctNum, client.lastName,
-			client.firstName, client.balance);
-		printf("Enter charge ( + ) or payment ( - ): ");
-		scanf("%lf", &transaction);
-		client.balance += transaction;
-		printf("%-6d%-16s%-11s%10.2f\n",
-			client.acctNum, client.lastName,
-			client.firstName, client.balance);
-		fseek(fPtr,
-			(account - 1) * sizeof(struct clientData),
-			SEEK_SET);
-		fwrite(&client, sizeof(struct clientData), 1,
-			fPtr);
-	}
+
+	printf("%-6d%-16s%-11s%10.2f\n\n",
+		s_clients[index].acctNum, s_clients[index].lastName,
+		s_clients[index].firstName, s_clients[index].balance);
+	printf("Enter charge ( + ) or payment ( - ): ");
+	scanf("%lf", &transaction);
+	s_clients[index].balance += transaction;
+	printf("%-6d%-16s%-11s%10.2f\n",
+		s_clients[index].acctNum, s_clients[index].lastName,
+		s_clients[index].firstName, s_clients[index].balance);
+
+	fseek(fPtr,
+		index * sizeof(ClientData),
+		SEEK_SET);
+	fwrite(&s_clients[index], sizeof(ClientData), 1,
+		fPtr);
 }
 
 void deleteRecord(FILE *fPtr)
 {
-	struct clientData client,
+	ClientData client,
 		blankClient = { 0, "", "", 0 };
 	int accountNum;
 
 	printf("Enter account number to "
 		"delete ( 1 - 100 ): ");
 	scanf("%d", &accountNum);
-	fseek(fPtr,
-		(accountNum - 1) * sizeof(struct clientData),
-		SEEK_SET);
-	fread(&client, sizeof(struct clientData), 1, fPtr);
 
-	if (client.acctNum == 0)
+	int index = recordIndex(accountNum);
+
+	if (-1 == index)
 	{
 		printf("Account %d does not exist.\n", accountNum);
+		return;
 	}
-	else 
-	{
-		fseek(fPtr,
-			(accountNum - 1) * sizeof(struct clientData),
-			SEEK_SET);
-		fwrite(&blankClient,
-			sizeof(struct clientData), 1, fPtr);
-	}
+
+	fseek(fPtr, index * sizeof(ClientData), SEEK_SET);
+	fwrite(&blankClient,
+		sizeof(ClientData), 1, fPtr);
+	//更新内存数据
+	s_clients[index].acctNum = 0;
+	
 }
 
 void newRecord(FILE *fPtr)
 {
-	struct clientData client = { 0, "", "", 0.0 };
+	ClientData client = { 0, "", "", 0.0 };
 	int accountNum;
 	printf("Enter new account number ( 1 - 100 ): ");
 	scanf("%d", &accountNum);
-	fseek(fPtr,
-		(accountNum - 1) * sizeof(struct clientData),
-		SEEK_SET);
-	fread(&client, sizeof(struct clientData), 1, fPtr);
-
-	if (client.acctNum != 0)
+	int index = recordIndex(accountNum);
+	if (-1 != index)
 	{
 		printf("Account #%d already contains information.\n",
 			client.acctNum);
+		return;
 	}
-	else 
+
+	printf("Enter lastname, firstname, balance\n? ");
+	scanf("%s%s%lf", &client.lastName, &client.firstName,
+		&client.balance);
+	client.acctNum = accountNum;
+	fseek(fPtr, 0, SEEK_END);
+	fwrite(&client,
+		sizeof(ClientData), 1, fPtr);
+
+	//更新内存数据
+	index = 0; 
+	while (s_clients[index].acctNum != 0)
 	{
-		printf("Enter lastname, firstname, balance\n? ");
-		scanf("%s%s%lf", &client.lastName, &client.firstName,
-			&client.balance);
-		client.acctNum = accountNum;
-		fseek(fPtr, (client.acctNum - 1) *
-			sizeof(struct clientData), SEEK_SET);
-		fwrite(&client,
-			sizeof(struct clientData), 1, fPtr);
+		index++;
 	}
+	s_clients[index] = client;
 }
 
 int enterChoice(void)
